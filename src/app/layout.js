16a384dd88a9d765a78875978109806a0ef633e4
Takanori88 <32048508+takanori88@users.noreply.github.com>
2025-04-28 17:42:37 +0900

    add icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,25 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Header from "./components/Header";
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import Header from './components/Header';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin']
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin']
 });
 
 export const metadata = {
-  title: "Who we are | when we ask.",
+  title: 'Who we are | when we ask.',
   description:
-    "It is a story that feels like a joke, and at the same time, like a quiet miracle.",
+    'It is a story that feels like a joke, and at the same time, like a quiet miracle.',
+  icons: {
+    icon: '/favicon.ico',
+    apple: '/apple-touch-icon.png'
+  }
 };
 
 export default function RootLayout({ children }) {
